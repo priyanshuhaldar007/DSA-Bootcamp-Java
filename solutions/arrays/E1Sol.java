bc1fb34edc203d4b8807ecf4491704951f6689e5
Priyanshu Haldar <priyanshu1843@gmail.com>
2022-09-25 18:38:37 +0530

    moving solutions directory

--- solutions/arrays/E1Sol.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] buildArray(int[] nums) {
-        int[] ans;
-        for(int i=0;i<nums.length;i++){
-            ans[i]= nums[nums[i]];
-        }
-        return ans;
-    }
-}
